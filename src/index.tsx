5366a81f7c0df0048e49beb6d7273b532797d851
Wesley.Rast <wesley.rast@world50.com>
2025-02-10 12:46:41 -0500

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,31 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const port = parseInt(process.env.PORT || "3000");
+const app = new Hono();
+
+const View = (props) => {
+  const styles = {
+    color: "#f30",
+  };
+
+  return (
+    <html>
+      <head></head>
+      <body>
+        <h1 style={styles}>Hello world</h1>
+      </body>
+    </html>
+  );
+};
+
+app.get("/", (c) => {
+  return c.html(<View />);
+});
+
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
